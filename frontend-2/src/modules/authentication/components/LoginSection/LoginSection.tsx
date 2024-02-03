99f28c7f1c1c95f304e980e1acc16a84a694e291
Mohamed <m.hamidi571@gmail.com>
2024-02-04 00:51:45 +0100

    [frontend] error handling ui for login

--- frontend-2/src/modules/authentication/components/LoginSection/LoginSection.tsx
+++ frontend-2/src/modules/authentication/components/LoginSection/LoginSection.tsx
@@ -4,9 +4,21 @@ import { useState } from "react";
 import { useAppDispatch } from "hooks/useAppDispatch";
 import LoginUser from "modules/authentication/services/login.services";
 import { Form } from "common/container/Form";
+import { useAppSelector } from "hooks/useAppSelector";
+import axios from "axios";
+import { getCookie } from "modules/authentication/utils/authHelper";
 
 const LoginSection = () => {
   const dispatch = useAppDispatch();
+
+  const { error } = useAppSelector((state) => state.login) as {
+    error: {
+      data: {
+        status: string;
+        message: string;
+      };
+    };
+  };
   const [loginData, setLoginData] = useState({
     email: "",
     password: "",
@@ -20,21 +32,88 @@ const LoginSection = () => {
     });
   };
 
+  const loginWithAxios = async () => {
+    await axios
+      .get("http://localhost:8000/sanctum/csrf-cookie", {
+        withCredentials: true,
+      })
+      .then(() => {
+        axios
+          .post("http://localhost:8000/api/auth/login", loginData, {
+            withCredentials: true,
+            headers: {
+              "X-XSRF-TOKEN": getCookie(),
+            },
+          })
+          .then((res) => {
+            console.log("login response ", res);
+          })
+          .catch((err) => {
+            console.log("login error ", err);
+          });
+      })
+      .catch((err) => {
+        console.log("COOKIE AUTH ERROR", err);
+      });
+  };
+
+  const loginWithFetchAPI = () => {
+    fetch("http://localhost:8000/sanctum/csrf-cookie", {
+      credentials: "include",
+      redirect: "manual",
+    })
+      .then((res) => {
+        console.log("session response 1 => ", res);
+        fetch("http://localhost:8000/api/auth/login", {
+          headers: {
+            "X-XSRF-TOKEN": getCookie(),
+          },
+          redirect: "manual",
+          method: "POST",
+          credentials: "include",
+          body: JSON.stringify(loginData),
+        })
+          .then((res) => {
+            if (res.redirected) return fetch(res.url);
+            console.log("login response => ", res);
+            return res.json();
+          })
+          .then((data) => {
+            console.log("FINAL DATA ", data);
+          })
+          .catch((error) => console.error("Error fetching data:", error));
+      })
+      .then((res) => {
+        console.log("res2", res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     dispatch(LoginUser(loginData));
+    // loginWithAxios()
+    // loginWithFetchAPI();
   };
 
   return (
     <div className={styles.loginContainer}>
       <h2>Login</h2>
       <Form onSubmit={handleLogin}>
+        {error && error.data ? (
+          <div className={styles.errorContainer}>
+            <h2 className={styles.errorMessage}>{error.data.message}</h2>
+          </div>
+        ) : null}
         <TextField
           value={loginData.email}
           onChange={handleChange}
           name="email"
           label="Email"
           type="email"
+          error={error ? true : false}
         />
         <TextField
           value={loginData.password}
@@ -42,6 +121,7 @@ const LoginSection = () => {
           name="password"
           label="Password"
           type="password"
+          error={error ? true : false}
         />
         <Button color="primary" variant="filled" width={10}>
           Login
