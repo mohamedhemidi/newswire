dc9303bb5cd2165f3daa6e69cf05fae5f5dc8269
Mohamed <m.hamidi571@gmail.com>
2024-02-18 22:44:37 +0100

    [frontend] login unit test restructuring

--- frontend/src/modules/authentication/__test__/LoginSection.test.tsx
+++ frontend/src/modules/authentication/__test__/LoginSection.test.tsx
@@ -1,34 +1,57 @@
 import { describe, it, expect } from "vitest";
 import { LoginSection } from "../components/LoginSection";
 import { fireEvent, render, screen } from "utils/test-utils";
+import { HttpResponse, http } from "msw";
+import { PATH } from "constants/environment";
+import { server } from "src/test/mocks/server";
+
+afterEach(() => {
+  server.resetHandlers();
+});
 
 describe("Login Section", () => {
+  it("renders error when login with Wrong credentials", async () => {
+    server.use(
+      http.post(PATH.userLogin, () => {
+        return HttpResponse.json(
+          { message: "Credentials do not match" },
+          { status: 401 }
+        );
+      })
+    );
 
-  it.skip("login successfully with correct credentials", async () => {
     render(<LoginSection />);
-    // Empty credentials
+
+    // Put Empty credentials :
     const emailInput = screen.getByTestId("email_field");
     const passwordInput = screen.getByTestId("password_field");
 
-    fireEvent.change(emailInput, {
-      target: { value: "mark@myer.com" },
-    });
-    fireEvent.change(passwordInput, { target: { value: "12345678" } });
+    fireEvent.change(emailInput, { target: { value: "mark@myer.com" } });
+    fireEvent.change(passwordInput, { target: { value: "123456789" } });
 
     expect(emailInput).toHaveValue("mark@myer.com");
-    expect(passwordInput).toHaveValue("12345678");
+    expect(passwordInput).toHaveValue("123456789");
 
     const loginButton = screen.getByRole("button", { name: "Login" });
     fireEvent.click(loginButton);
 
     // Login Error
-    const successDiv = await screen.findByTestId("success");
+    const errorText = await screen.findByText(/credentials do not match/i);
+    const errorDiv = await screen.findByTestId("error");
     screen.debug();
-    expect(successDiv).toBeInTheDocument();
+    expect(errorDiv).toBeInTheDocument();
+    expect(errorText).toBeInTheDocument();
   });
 
-
-  it("renders error when login with empty credentials", async () => {
+  it("renders error when login with Empty credentials", async () => {
+    server.use(
+      http.post(PATH.userLogin, () => {
+        return HttpResponse.json(
+          { message: "The email field is required. (and 1 more error)" },
+          { status: 422 }
+        );
+      })
+    );
     render(<LoginSection />);
     // Empty credentials
     const emailInput = screen.getByTestId("email_field");
@@ -43,8 +66,10 @@ describe("Login Section", () => {
     fireEvent.click(loginButton);
 
     // Login Error
+    const errorText = await screen.findByText(/the email field is required/i);
     const errorDiv = await screen.findByTestId("error");
     screen.debug();
+    expect(errorText).toBeInTheDocument();
     expect(errorDiv).toBeInTheDocument();
   });
 });
--- frontend/src/test/mocks/handlers.ts
+++ frontend/src/test/mocks/handlers.ts
@@ -8,26 +8,14 @@ export const handlers = [
   http.post(PATH.userLogin, () => {
     return HttpResponse.json(
       {
-        message: "The email field is required. (and 1 more error)",
-      },
-      { status: 422 }
-    );
-  }),
-  http.post(PATH.userLogin, () => {
-    return HttpResponse.json(
-      {
         status: "Request was successful.",
-        message: null,
         data: {
           user: {
             id: 3,
             name: "Mark",
             email: "mark@myer.com",
             email_verified_at: null,
-            created_at: "2023-12-04T02:34:28.000000Z",
-            updated_at: "2023-12-04T02:34:28.000000Z",
           },
-          token: "286|hCeCTn8ljJ4i7WDDePHt8fgKWr2Linu1EDlsTIDb269df61f",
         },
       },
       { status: 200 }
